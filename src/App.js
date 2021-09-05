4a1606ab9ede39a4a9fcb9234e10c5e167685ab1
ionuraygan <ionuraygan@gmail.com>
2021-09-06 00:19:54 +0300

    Create components and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
  import './App.css';
 import Header from './Components/Header';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Home } from './Home';
+import { Messages } from './Messages';
+import { Users } from './Users';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header/>
     </div>
+      <Route path = "/" component={Home} />
+      <Route path = "/Messages" component={Messages} />
+      <Route path = "/Users" component={Users} />
+    </Router>
   );
 }
 
