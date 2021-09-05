eaa8907e08add8baeb6a3e38db2786f436bafef6
ionuraygan <ionuraygan@gmail.com>
2021-09-06 01:23:32 +0300

    Get location object that represents the current URL by useLocation hook

--- src/Post.js
+++ src/Post.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { useParams } from 'react-router'
+import { useLocation, useParams } from 'react-router'
 
 export const Post = ({match}) => {
 
     const { id } = useParams();
 
+    const query = new URLSearchParams(useLocation().search)
+
     return (
         <div>
-            <h2>Id is = {match.params.id} </h2>
+            <h2>Id is = {id} </h2>
+            <h2>{query.get("name")}</h2>
+            <h2>{query.get("surname")}</h2>
+
         </div>
     )
 }
